ac60b69effa41d63f89579cf30200b5a8b6b8e14
Dhanushree-13 <2025cs_dhanushreek_b@nie.ac.in>
2026-09-22 12:58:02 +0530

    Adding multiple roots

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -1,5 +1,20 @@
+#HTTP Methods:Are a way to talk (From browser) to the server
+#methods 
+#GET : Read data
+#POST:create a new data
+#PUT : Replace existing data
+#PATCH : Partial update
+#DELETE : Remove data
 from fastapi import FastAPI
 app=FastAPI()
 @app.get("/")
-def read_root():
-    return {"message":"Hello World","number":44,"is fun":True}
+def home():
+    return{"page":"Home"} 
+
+@app.get("/about")
+def about():
+     return{"page":"About","author":"Dhanu"}
+
+@app.get("/health")
+def health():
+     return("Status":"ok") 
