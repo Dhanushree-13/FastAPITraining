814cee14dd561acb71d6ea28de883d842f8f31e3
Dhanushree-13 <2025cs_dhanushreek_b@nie.ac.in>
2026-09-22 15:22:37 +0530

    Pydantic Model

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -6,6 +6,7 @@
 #PATCH : Partial update
 #DELETE : Remove data
 from fastapi import FastAPI
+from pydantic import BaseModel
 app=FastAPI()
 @app.get("/")
 def home():
@@ -17,4 +18,27 @@ def about():
 
 @app.get("/health")
 def health():
-     return("Status":"ok") 
+     return {"Status":"ok"}
+
+#POST request
+@app.post("/create")
+def create_something():
+     return {"message":"Created"}
+#Path parameters
+@app.get("/student/{roll}")
+def get_result(roll):
+     return {"Result":"Distinction","roll":roll}
+#Path Parameters with type hint
+@app.get("/candidate/{roll}")
+def get_Candidate(roll:int):
+     return {"Result":"Distinction","roll":roll,"type":str(type(roll))}
+
+#Pydantic model
+class Item(BaseModel):
+     name:str
+     price:float
+     in_stock:bool=True
+
+@app.post("/items")
+def create_item(item:Item):
+     return {"received":item,"total_price":item.price*1.18}
